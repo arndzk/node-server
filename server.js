cc8f7d3f400cc41721960e66b3703d09bd7e6cd9
CodexAcquillae <arn.dziudzik@gmail.com>
2019-03-14 16:15:55 +0200

    Added Express Middleware

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+    console.log('<h1>Hello World!</h1>');
+    next();
+})
 
 app.get('/', (req, res) => {
     res.send("Getting Root...");
@@ -11,11 +20,8 @@ app.get('/profile', (req, res) => {
 });
 
 app.post('/profile', (req, res) => {
-    const user = {
-        name: 'Johnny',
-        surname: 'Five'
-    }
-    res.send(user);
+    console.log(req.body);
+    res.send('Success!');
 });
 
 app.listen(3000);
