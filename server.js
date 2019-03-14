63cc69f4688c7ae357de029a0bed878ee3ab5880
CodexAcquillae <arn.dziudzik@gmail.com>
2019-03-14 17:16:42 +0200

    Updated RESTful API

--- server.js
+++ server.js
@@ -2,26 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.use(express.static(__dirname + '/public'));
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
-
-app.use((req, res, next) => {
-    console.log('<h1>Hello World!</h1>');
-    next();
-})
-
-app.get('/', (req, res) => {
-    res.send("Getting Root...");
-});
-
-app.get('/profile', (req, res) => {
-    res.send("Getting Profile...");
-});
-
-app.post('/profile', (req, res) => {
-    console.log(req.body);
-    res.send('Success!');
-});
 
 app.listen(3000);
