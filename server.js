9e1c6dda790be832b3c8ab220e253c8ddcbcf2a4
CodexAcquillae <arn.dziudzik@gmail.com>
2019-03-14 15:54:40 +0200

    Added Express

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
-const http = require('http');
+const express = require('express');
 
-const server = http.createServer((request, response) => {
-    console.log('headers', request.headers);
-    console.log('method', request.method);
-    console.log('url', request.url);
+const app = express();
 
+app.get('/', (req, res) => {
+    res.send("Getting Root...");
+});
+
+app.get('/profile', (req, res) => {
+    res.send("Getting Profile...");
+});
+
+app.post('/profile', (req, res) => {
     const user = {
         name: 'Johnny',
         surname: 'Five'
     }
+    res.send(user);
+});
 
-    response.setHeader('Content-Type', 'application/json');
-    response.end(JSON.stringify(user));
-})
-
-server.listen(3000);
+app.listen(3000);
